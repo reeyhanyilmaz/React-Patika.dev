60e3ee5226d7b89919a6fbef9aefaddccfa9e78b
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-04-10 11:43:07 +0300

    add Real Time Çalışan Uygulamalar Geliştirmek

---
+++ real-time-uygulama-gelistirme/client/src/index.js
@@ -0,0 +1,35 @@
+
+//React 17**********************************************************************************
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+
+
+//react  18 ile güncellendi*********************************************************************
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
+
+reportWebVitals();
